b51eef1930c1d689eaa9d086f9d12e1b64a11db9
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-27 21:57:42 +0000

    update task mgr app 1

--- taskmgr-app-v01/src/App.js
+++ taskmgr-app-v01/src/App.js
@@ -3,11 +3,13 @@
 import {HashRouter, Routes, Route} from "react-router-dom"
 import Home from "./components/Home";
 import ContactUs from "./components/ContactUs";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <HashRouter>            
+      <HashRouter>    
+        <NavBar/>        
         <Routes>      
           <Route path="/" element={<Home/>}/>
           <Route path="/contactus" element={<ContactUs/>}/>
