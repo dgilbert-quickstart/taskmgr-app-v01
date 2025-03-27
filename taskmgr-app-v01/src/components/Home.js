25e3e6224a4b315c0b00b80b53abfa09ee97f087
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-27 22:01:45 +0000

    update task mgr app 1

--- taskmgr-app-v01/src/components/Home.js
+++ taskmgr-app-v01/src/components/Home.js
@@ -1,8 +1,14 @@
+import {Link} from "react-router-dom"
+
 function Home() {
     return (
       <>
         <p>Demo Task Manager App v1.0.5</p>
+        <p></p>
+
+        <Link to="/contactus">Contact Us</Link>
       </>
+
     );
   }
   
