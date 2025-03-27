b64b81bd0d64474d905bcd4b729b8290c7c0a486
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-27 22:34:56 +0000

    update task mgr app 1

--- taskmgr-app-v01/src/components/ContactUs.js
+++ taskmgr-app-v01/src/components/ContactUs.js
@@ -2,6 +2,17 @@ function ContactUs() {
     return (
       <>
         <p>Contact Us</p>
+        <form>
+            <div> 
+                <span>first name: </span> <input type="text" maxLength={25} placeholder="* first name"/>
+                <br/>
+                <span>last name: </span> <input type="text" maxLength={25} placeholder="last name"/>
+                <br/>
+                <span>email: </span> <input type="text" maxLength={100} placeholder="* email"/>
+                <p></p>
+                <button>submit</button> {" "} <a href="#">clear</a>
+            </div>
+        </form>
       </>
     );
   }
--- taskmgr-app-v01/src/components/Home.js
+++ taskmgr-app-v01/src/components/Home.js
@@ -1,12 +1,19 @@
 import {Link} from "react-router-dom"
+//import Header form "./components/Header"
 
 function Home() {
     return (
       <>
         <p>Demo Task Manager App v1.0.5</p>
         <p></p>
-
+        <div>
+            <input type="text" maxLength={25} placeholder="* add item"/>
+            {" "} <button>Add</button> {" "} <a>clear</a>
+            <p></p>
+        </div>
         <Link to="/contactus">Contact Us</Link>
+        <p></p>
+        <p>footer @ 2025</p>
       </>
 
     );
