96b5db1dcbd52ddabf6f253ba1da7cd6728fb514
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-28 16:08:13 +0000

    update react taskmgr app v01

--- taskmgr-app-v01/src/components/ContactUs.js
+++ taskmgr-app-v01/src/components/ContactUs.js
@@ -1,18 +1,24 @@
 function ContactUs() {
+
     return (
       <>
-        <p>Contact Us</p>
-        <form>
-            <div> 
-                <span>first name: </span> <input type="text" maxLength={25} placeholder="* first name"/>
-                <br/>
-                <span>last name: </span> <input type="text" maxLength={25} placeholder="last name"/>
-                <br/>
-                <span>email: </span> <input type="text" maxLength={100} placeholder="* email"/>
-                <p></p>
-                <button>submit</button> {" "} <a href="#">clear</a>
-            </div>
-        </form>
+      <div>
+          <h2>Contact Us</h2>
+          <form>
+              <div> 
+                  <span>* first name: </span> <input type="text" maxLength={25} placeholder="* first name"/>
+                  <br/>
+                  <span>last name: </span> <input type="text" maxLength={25} placeholder="last name"/>
+                  <br/>
+                  <span>* email: </span> <input type="text" maxLength={100} placeholder="* email"/>
+                  <br/>
+                  <span>* comment: </span> 
+                  <br/><textarea rows={5} cols={30} maxLength={100} placeholder="* comment"></textarea>
+                  <p></p>
+                  <button>submit</button> {" "} <a>clear</a>
+              </div>
+          </form>
+      </div>
       </>
     );
   }
--- taskmgr-app-v01/src/components/Home.js
+++ taskmgr-app-v01/src/components/Home.js
@@ -1,19 +1,29 @@
-import {Link} from "react-router-dom"
-//import Header form "./components/Header"
-
 function Home() {
-    return (
+
+  return (
       <>
-        <p>Demo Task Manager App v1.0.5</p>
-        <p></p>
-        <div>
-            <input type="text" maxLength={25} placeholder="* add item"/>
-            {" "} <button>Add</button> {" "} <a>clear</a>
-            <p></p>
+      <div> 
+          <h3>Demo Task Manager App v1.0.6</h3>
+          <p></p>
+          <div>
+              <input type="text" maxLength={25} placeholder="* add item"/>
+              {" "} <button>Add</button> {" "} <a>clear</a>
+              <p></p>
+          </div>
+          <p></p>
+          <a>all</a> {" | "}
+          <a>complete</a> {" | "}
+          <a>in-complete</a> 
+          <p></p>
+          <div>
+            <span>[] itema -x- </span><br/>
+            <span>[x] itemb -x- </span><br/>
+            <span>[] itemc -x- </span><br/>
+            <span>[x] itemd -x- </span><br/>
+          </div>
+          <p></p>
+          <p>footer @ 2025</p>       
         </div>
-        <Link to="/contactus">Contact Us</Link>
-        <p></p>
-        <p>footer @ 2025</p>
       </>
 
     );
